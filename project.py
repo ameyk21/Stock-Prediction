984a0858e2737ddeb05f20ac1a055c809666fcdb
ameyk21 <111609594+ameyk21@users.noreply.github.com>
2022-08-20 14:12:28 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-import multiprocessing
+#importing all the necessary modules
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -14,28 +14,29 @@ from tensorflow.keras import layers
 df =pd.read_csv('TTM.csv')
 stock_data=df.copy()
 
+#making an 80% closing value dataset after turing it into a number series
 close_prices = stock_data['Close']
 values = close_prices.values
 training_data_len = math.ceil(len(values)* 0.8)
 
-
+#normalising the closing data and turind the scaled data into a 2D array and the first 80% of the dataset is filled into train_data
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(values.reshape(-1,1))
 train_data = scaled_data[0: training_data_len, :]
-
+#lists for feature data and label data
 x_train = []
 y_train = []
-
+#filling 60 days of historical data into the feature and label lists
 for i in range(60, len(train_data)):
     x_train.append(train_data[i-60:i, 0])
     y_train.append(train_data[i, 0])
     
+#converting the feature and label lists into numpy arrays and reshaping feature data array into 3D array for tensorflow
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 
-
-
+#making a test data 3D array from the latest 20% of closing value data
 test_data = scaled_data[training_data_len-60: , : ]
 x_test = []
 y_test = values[training_data_len:]
@@ -46,7 +47,7 @@ for i in range(60, len(test_data)):
 x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-
+# a sequential model is defined with two LSTM layers having a 100 network units each. A neural network layer with 25 netowrk units is added
 model = keras.Sequential()
 model.add(layers.LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model.add(layers.LSTM(100, return_sequences=False))
